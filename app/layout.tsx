0ad37ba65603e33b5d99ff2ee54b8b37132f184a
Kerimkulov Samat <samatkerimkulov1@gmail.com>
2026-07-21 00:37:24 -0500

    fix: size footer link accessibly

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,5 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><head><link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /><link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap" rel="stylesheet" /></head><body><ThemeManager />{children}<footer className="site-footer"><a href="/credits">Credits</a></footer></body></html>;
+  return <html lang="en"><head><link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /><link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap" rel="stylesheet" /></head><body><ThemeManager />{children}<footer className="site-footer"><a className="site-footer-link" href="/credits">Credits</a></footer></body></html>;
 }
